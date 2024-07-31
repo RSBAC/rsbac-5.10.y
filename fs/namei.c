e55d25054e2149928d8e2d30bf958de7b008901a
Amon Ott <ao@rsbac.org>
2024-07-31 09:39:34 +0200

    Add feature CAP filesystem object hiding If enabled, you can hide filesystem objects from users without Linux read rights on them, set with the kernel command line switch rsbac_cap_fd_hiding or through the RSBAC proc interface. There are two possible values: 0 / off: no hiding. 1 / on: only processes with Linux read right to a filesystem object,     Linux capability DAC_OVERRIDE or DAC_READ_SEARCH or with CAP role     security officer or system admin may see this object. Hiding works with many, but not all types of file system object access.
    While at it, reduce overhead with CAP process hiding: only use global
    variable rsbac_cap_process_hiding instead of per-process attribute,
    which does not get set individually anyway.
    Since rsbac_get_owner() never returns an error, change it to not return
    a result and remove all result checks.
    Add some type casts to rc_main.c.
    vfs_getattr(): with RSBAC_FSOBJ_HIDE, avoid SEARCH request on IPC targets.

--- fs/namei.c
+++ fs/namei.c
@@ -1542,6 +1542,10 @@ static struct dentry *lookup_fast(struct nameidata *nd,
 	}
 
 #ifdef CONFIG_RSBAC
+#if defined(CONFIG_RSBAC_CAP_FD_HIDE)
+	if (rsbac_cap_hide_fd(dentry))
+		return ERR_PTR(-ENOENT);
+#endif
 	if (   dentry
 	    && dentry->d_inode
 	    && dentry->d_inode->i_sb
@@ -1707,6 +1711,10 @@ static const char *pick_link(struct nameidata *nd, struct path *link,
 		return ERR_PTR(error);
 
 #ifdef CONFIG_RSBAC
+#if defined(CONFIG_RSBAC_CAP_FD_HIDE)
+	if (rsbac_cap_hide_fd(link->dentry))
+		return ERR_PTR(-ENOENT);
+#endif
 	if (link->dentry->d_sb && link->dentry->d_inode) {
 		rsbac_target_id.symlink.device = link->dentry->d_sb->s_dev;
 		rsbac_target_id.symlink.inode  = link->dentry->d_inode->i_ino;
@@ -2243,6 +2251,10 @@ static int link_path_walk(const char *name, struct nameidata *nd)
 			return err;
 
 #ifdef CONFIG_RSBAC
+#if defined(CONFIG_RSBAC_CAP_FD_HIDE)
+		if (rsbac_cap_hide_fd(nd->path.dentry))
+			return -ENOENT;
+#endif
 		if (nd->inode->i_sb) {
 			rsbac_target_id.dir.device = nd->inode->i_sb->s_dev;
 			rsbac_target_id.dir.inode  = nd->inode->i_ino;
@@ -2334,6 +2346,10 @@ OK:
 			}
 			return -ENOTDIR;
 		}
+#if defined(CONFIG_RSBAC_CAP_FD_HIDE)
+		if (rsbac_cap_hide_fd(nd->path.dentry))
+			return -ENOENT;
+#endif
 #ifdef CONFIG_RSBAC_FSOBJ_HIDE
 		if (nd->inode->i_sb) {
 			rsbac_target_id.dir.device = nd->inode->i_sb->s_dev;
@@ -3628,6 +3644,16 @@ static int do_open(struct nameidata *nd,
 		if (error)
 			return error;
 	}
+
+#if defined(CONFIG_RSBAC_CAP_FD_HIDE)
+	if (   nd->path.dentry
+	    && nd->path.dentry->d_inode
+	    && !S_ISBLK(nd->path.dentry->d_inode->i_mode)
+	    && !S_ISCHR(nd->path.dentry->d_inode->i_mode)
+	    && rsbac_cap_hide_fd(nd->path.dentry))
+		return -ENOENT;
+#endif
+
 	if (!(file->f_mode & FMODE_CREATED))
 		audit_inode(nd->name, nd->path.dentry, 0);
 	if (open_flag & O_CREAT) {
@@ -3711,6 +3737,17 @@ static int do_open(struct nameidata *nd,
 						rsbac_adf_req = R_READ_OPEN;
 		if ((rsbac_adf_req != R_NONE) && (rsbac_target != T_NONE)) {
 			rsbac_attribute_value.open_flag = open_flag;
+#ifdef CONFIG_RSBAC_FSOBJ_HIDE
+			if (   rsbac_target != T_DEV && rsbac_target != T_IPC
+			    && !rsbac_adf_request(R_SEARCH,
+						task_pid(current),
+						rsbac_target,
+						rsbac_target_id,
+						A_open_flag,
+						rsbac_attribute_value)) {
+				error = -ENOENT;
+			} else
+#endif
 			if (!rsbac_adf_request(rsbac_adf_req,
 						task_pid(current),
 						rsbac_target,
