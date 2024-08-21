169be9ce9f1396b726b3234b2603996bba07e4be
Amon Ott <ao@rsbac.org>
2024-08-21 09:39:27 +0200

    With CAP fd hiding, also do not hide with write access.

--- rsbac/help/helpers.c
+++ rsbac/help/helpers.c
@@ -3,7 +3,7 @@
 /* Author and (c) 1999-2024:           */
 /*   Amon Ott <ao@rsbac.org>           */
 /* Helper functions for all parts      */
-/* Last modified: 20/Aug/2024          */
+/* Last modified: 21/Aug/2024          */
 /************************************* */
 
 #include <rsbac/types.h>
@@ -517,6 +517,8 @@ rsbac_boolean_t rsbac_cap_hide_fd(struct dentry * target_dentry)
 	if (uid_eq(target_dentry->d_inode->i_uid, current_fsuid()))
 		return FALSE;
 	if (!generic_permission(target_dentry->d_inode, MAY_READ))
+		return FALSE;
+	if (!generic_permission(target_dentry->d_inode, MAY_WRITE))
 		return FALSE;
 
 	rsbac_get_owner(&rsbac_target_id.user);
