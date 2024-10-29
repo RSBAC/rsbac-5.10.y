90d55dcf47ad2458749413e0496dfc20ffed0db7
Amon Ott <ao@rsbac.org>
2024-10-29 09:58:50 +0100

    Add request types GET_XATTR and MODIFY_XATTR on FD targets for get/setxattr.

--- fs/xattr.c
+++ fs/xattr.c
@@ -278,7 +278,7 @@ __vfs_setxattr_locked(struct dentry *dentry, const char *name,
 	rsbac_target_id.file.inode  = inode->i_ino;
 	rsbac_target_id.file.dentry_p = dentry;
 	rsbac_attribute_value.dummy = 0;
-	if (!rsbac_adf_request(R_MODIFY_PERMISSIONS_DATA,
+	if (!rsbac_adf_request(R_MODIFY_XATTR,
 				task_pid(current),
 				rsbac_target,
 				rsbac_target_id,
@@ -444,7 +444,7 @@ vfs_getxattr(struct dentry *dentry, const char *name, void *value, size_t size)
 	rsbac_target_id.file.inode  = inode->i_ino;
 	rsbac_target_id.file.dentry_p = dentry;
 	rsbac_attribute_value.dummy = 0;
-	if (!rsbac_adf_request(R_GET_PERMISSIONS_DATA,
+	if (!rsbac_adf_request(R_GET_XATTR,
 				task_pid(current),
 				rsbac_target,
 				rsbac_target_id,
@@ -502,7 +502,7 @@ vfs_listxattr(struct dentry *dentry, char *list, size_t size)
 	rsbac_target_id.file.inode  = inode->i_ino;
 	rsbac_target_id.file.dentry_p = dentry;
 	rsbac_attribute_value.dummy = 0;
-	if (!rsbac_adf_request(R_GET_PERMISSIONS_DATA,
+	if (!rsbac_adf_request(R_GET_XATTR,
 				task_pid(current),
 				rsbac_target,
 				rsbac_target_id,
@@ -583,7 +583,7 @@ __vfs_removexattr_locked(struct dentry *dentry, const char *name,
 	rsbac_target_id.file.inode  = inode->i_ino;
 	rsbac_target_id.file.dentry_p = dentry;
 	rsbac_attribute_value.dummy = 0;
-	if (!rsbac_adf_request(R_MODIFY_PERMISSIONS_DATA,
+	if (!rsbac_adf_request(R_MODIFY_XATTR,
 				task_pid(current),
 				rsbac_target,
 				rsbac_target_id,
